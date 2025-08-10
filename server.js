0a11d02519fe30b07893fe1bbe21d1abe8e71a47
Malachi Cohen <malachicohen@gmail.com>
2025-08-10 13:41:37 +0300

    Add server.js with basic Express server setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Basic route for the root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Hello World web app listening at http://localhost:${port}`);
+});
